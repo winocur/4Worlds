fe4a3e1e6738fa0c417f92f48d4c815d71ca9397
Mwinocur <manuelwinocur@gmail.com>
2019-05-02 23:19:26 -0300

    init commit

---
+++ Assets/Scripts/FPPCharacterController.cs
@@ -0,0 +1,108 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FPPCharacterController : MonoBehaviour
+{
+    public Transform feet;
+
+    Rigidbody rigidBody;
+    float jumpForce = 6f;
+    float accelerationSpeed = 5f;
+    float horizontalSpeed = 3f;
+    float groundedDistance = 0.3f;
+    float airControlMovement = 1f;
+
+    float momentumModifier = 0.06f;
+    float momentumDeacceleration = 0.03f;
+    public float forwardMomentum = 1f;
+
+    public Vector3 lastMovement;
+
+    [SerializeField]
+    private bool isGrounded = true; 
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.rigidBody = this.GetComponent<Rigidbody>();
+    }
+
+    void OnDrawGizmos()
+    {
+           Gizmos.DrawWireSphere(feet.position - new Vector3(0, 0.55f, 0), 0.55f);
+     
+    }
+
+    void Update ()
+    {
+        RaycastHit hit;
+        if(Physics.SphereCast(feet.position, 0.55f, Vector3.down, out hit, 0.55f, LayerMask.GetMask("Floor")))
+        {
+            isGrounded = true;
+        }
+        else
+        {
+            isGrounded = false;
+        }
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        
+        Vector3 currentPosition = this.transform.position;
+
+        float rotationY = this.transform.rotation.eulerAngles.y;
+        float verticalInput = Input.GetAxisRaw("Vertical");
+        float horizontalInput = Input.GetAxisRaw("Horizontal");
+
+        Vector3 movement;
+
+        forwardMomentum = Mathf.Clamp(forwardMomentum + (verticalInput * momentumModifier * Time.fixedDeltaTime) 
+                                    - momentumDeacceleration * Time.fixedDeltaTime,
+                                    1f, 2.5f);
+
+        if(isGrounded)
+        {
+            movement = (Quaternion.AngleAxis(rotationY, Vector3.up) * 
+                    new Vector3(horizontalInput * horizontalSpeed * Time.fixedDeltaTime
+                            ,0
+                            ,verticalInput * accelerationSpeed * forwardMomentum * Time.fixedDeltaTime)
+                    );
+
+        }
+        else
+        {
+           movement = (Quaternion.AngleAxis(rotationY, Vector3.up) * 
+                    new Vector3(horizontalInput * airControlMovement * Time.fixedDeltaTime
+                            ,0
+                            ,verticalInput * airControlMovement * Time.fixedDeltaTime)
+                    ); 
+        }
+
+        this.rigidBody.MovePosition(currentPosition + movement);
+        
+        lastMovement = movement;
+
+        if(Input.GetButtonDown("Jump")) {
+            if(this.isGrounded) {
+                this.rigidBody.AddForce( (Vector3.up + 
+                                    (lastMovement.normalized / 2))
+                                     * forwardMomentum 
+                                     * jumpForce, ForceMode.Impulse);
+            }
+        }
+
+    }
+
+    public void PushBounce ()
+    {
+
+    }
+
+    public void PushInertia ()
+    {
+
+    }
+}
---
+++ Assets/Scripts/MovingPlatform.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovingPlatform : MonoBehaviour
+{
+    public Transform platform;
+    public Transform point_0;
+    public Transform point_1;
+
+    public float speed = 2f;
+    public Transform target;
+
+    Rigidbody rigidBody;
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = point_0;
+        this.rigidBody = this.GetComponentInChildren<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // target change
+        if((target.position - platform.position).magnitude < 0.1f)
+        {
+            target = (target == point_0) ? point_1 : point_0; 
+        }
+        rigidBody.MovePosition(platform.position +
+                        ((target.position - platform.position).normalized * speed * Time.deltaTime)
+                        );
+    }
+}
